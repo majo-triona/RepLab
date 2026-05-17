8adeb13cf51fea96f7705bde84d892510a8a5edd
Teo <168961146+majo-triona@users.noreply.github.com>
2026-05-17 23:41:53 +0300

    Integrate system

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include "User.h"
-#include <iostream>
+#include "Workout.h"
+#include "StrengthExercise.h"
+#include "Cardio.h"
 
 int main() {
     User u("Ivan", 20, 75, 180);
-    u.showUser();
+
+    Workout* w1 = new Workout();
+
+    w1->addExercise(new StrengthExercise("Bench Press", 80, 10));
+    w1->addExercise(new Cardio("Running", 30));
+
+    w1->showWorkout();
 
     return 0;
 }
